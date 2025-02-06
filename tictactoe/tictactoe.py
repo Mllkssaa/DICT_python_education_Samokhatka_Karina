a1542f0197f43d930cdabed26e51a30cdc6f5741
Mllkssaa <k.v.samokhatka@student.khai.edu>
2025-02-06 21:09:07 +0200

    tictactoe  {3-rd} stage

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -19,6 +19,45 @@ def playerInput(board):
     else:
         print("Oops player is already in that spot.=)")
 
+def checkHorizontle(board)
+    global winner
+    if board[0] == board[1] == board[2] and board[1] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
+
+
+def checkRow(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] !="-":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
+
+
+def checkDiag(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != "-":
+        winner board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != "-":
+        winner board[2]
+        return True
+
+
+
+
+
 
 while gameRunning:
     printBoard(board)
