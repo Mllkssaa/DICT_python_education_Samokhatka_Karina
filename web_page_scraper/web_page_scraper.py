13c128d44d68098251afce7a322f3aafa6d94665
Mllkssaa <k.v.samokhatka@student.khai.edu>
2025-05-25 14:43:33 +0300

    w_p_s {2-nd} stage

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -3,8 +3,8 @@ import requests
 import string
 from bs4 import BeautifulSoup
 
-def request():
-    url = input("Input the URL: \n>")
+def stage1():
+    url = input("Input the URL.\n>")
     try:
         response = requests.get(url)
         if response.status_code != 200:
@@ -19,4 +19,28 @@ def request():
     except Exception:
         print("Invalid quote resource..")
 
-request()
+
+def stage2():
+    url = input("Input the URL.\n>")
+    if "imdb.com/title/" not in url:
+        print("Invalid movie page.")
+        return
+
+    try:
+        response = requests.get(url, headers={'Accept-Language': 'en-US, en;q=0.5'})
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        title_tags = soup.find('title')
+        meta_desc = soup.find('meta', {'name': 'description'})
+
+        if title_tags and meta_desc:
+            title = title_tags.text.split('-')[0].strip()
+            description = meta_desc.get('content').strip()
+            print({"title": title, "description": description})
+        else:
+            print("Invalid movie page.")
+    except Exception:
+        print("Invalid movie page.")
+
+stage1()
+stage2()
