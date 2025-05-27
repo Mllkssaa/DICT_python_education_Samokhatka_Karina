34992b2c4ef617d8c9b8ab4f4ceee875a73f37e7
Mllkssaa <k.v.samokhatka@student.khai.edu>
2025-05-27 22:14:52 +0300

    curr_exc {1-st} stage

---
+++ currency_exchange/currency_exchange.py
@@ -0,0 +1,3 @@
+mycoins = float(input("Please, enter the number of mycoins you have. >"))
+rate = float(input("Please, enter the exchange rate. >"))
+print(f"The total amount of dollars.--> {round(mycoins * rate, 2)}")
